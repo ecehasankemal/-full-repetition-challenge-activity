48c30d9a276d7e229771a008f618ee4ef2df6709
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2024-09-17 09:25:43 +0300

    Update my_print_comb2.c

--- example_piscine_c00/ex06/my_print_comb2.c
+++ example_piscine_c00/ex06/my_print_comb2.c
@@ -3,47 +3,38 @@
 void
     my_print_comb2(void)
 {
-    // 00 00, 00 01, 00 02, 00 03, 00 04, 00 05, 00 06, 00 07, 00 08, 00 09
+    char arr[4] = {'0', '0', '0', '0'};
 
-    char a;
-    char b;
-    char c;
-    char d;
-
-    a = '0';
-    b = '0';
-    c = '0';
-    d = '0';
-
-    while (a <= '9')
+    while (arr[0] <= '9') // arr[0] ilk sayıyı temsil eder
     {
-        while (b <= '9')
+        while (arr[1] <= '9') // arr[1] ikinci sayıyı temsil eder
         {
-            c = a;
-            d = b + 1; // ilk sayıya göre ikinci sayıyı bir arttırıyoruz
-            while (c <= '9')
+            arr[2] = arr[0];
+            arr[3] = arr[1] + 1; // ilk sayıya göre ikinci sayıyı bir arttırıyoruz
+            while (arr[2] <= '9') // arr[2] üçüncü sayıyı temsil eder
             {
-                while(d <= '9')
+                while (arr[3] <= '9') // arr[3] dördüncü sayıyı temsil eder
                 {
-                    write(1, &a, 1);
-                    write(1, &b, 1);
+                    write(1, arr, 2);
                     write(1, " ", 1);
-                    write(1, &c, 1);
-                    write(1, &d, 1);
-                    if (!(a == '9' && b == '8' && c == '9' && d == '9'))
+                    write(1, arr + 2, 2);
+                    if (!(arr[0] == '9' && arr[1] == '8' && arr[2] == '9' && arr[3] == '9'))
                     {
                         write(1, ", ", 2);
                     }
-                    d++;
+                    arr[3]++;
                 }
-                d = '0';
-                c++;
+                arr[3] = '0';
+                arr[2]++;
             }
-            c = '0';
-            b++;
+            arr[2] = '0';
+            arr[1]++;
         }
-        b = '0';
-        a++;
+        arr[1] = '0';
+        arr[0]++;
     }
+    
     write(1, "\n", 1);
 }
+
+// 00 00, 00 01, 00 02, 00 03, 00 04, 00 05, 00 06, 00 07, 00 08, 00 09
